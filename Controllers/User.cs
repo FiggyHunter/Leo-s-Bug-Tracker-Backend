2317a0278c38ac10f9855ca130c4276a9648135d
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-13 13:40:37 +0100

    Testing the return of all users

---
+++ Controllers/User.cs
@@ -0,0 +1,55 @@
+﻿using BugTrackerAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+[Route("api/username")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly string _connectionString;
+
+    public UserController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        List<User> users = new List<User>();
+
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Users";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        User user = new User
+                        {
+                            Name = reader["Name"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            Role = reader["Role"].ToString(),
+                            Avatar = reader["Avatar"].ToString()
+                        };
+                        users.Add(user);
+                    }
+                }
+            }
+
+            return Ok(users);
+        }
+        catch (SqlException e)
+        {
+            // Log the exception
+            return StatusCode(500, "Internal server error: " + e.Message);
+        }
+    }
+}
