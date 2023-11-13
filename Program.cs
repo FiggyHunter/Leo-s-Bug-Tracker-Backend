e6d3ec47c65feab419a68e06d7039ea73dda4a47
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-13 18:38:48 +0100

    Added JWT Bearer

--- Program.cs
+++ Program.cs
@@ -1,17 +1,45 @@
-using BugTrackerAPI.Entities;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
+using BugTrackerAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters { 
+        ValidIssuer = config["Jwt:Issuer"],
+        ValidAudience = config["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"!])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+    };
+
+});
+builder.Services.AddMvc();
+builder.Services.AddControllers(); 
+builder.Services.AddScoped<IUserService, UserService>();
 
 
-var builder = WebApplication.CreateBuilder(args); 
-builder.Services.AddControllers();
-var app = builder.Build(); 
 
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => {
     return "Okay";
 });
+
 
 app.UseRouting();
 app.MapControllers();
