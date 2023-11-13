dd91d358857470827de8180ccba99a5fbd483cd0
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-13 18:40:09 +0100

    Started Working on Login Controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using BugTrackerAPI.Entities;
+using BugTrackerAPI.Models;
+using BugTrackerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BugTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUserService _userService;
+        private IConfiguration _configuration;
+        public LoginController(IConfiguration configuration, IUserService userService)
+        {   
+            _configuration = configuration; 
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+            if(user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+            return NotFound("User not found");
+        }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Dsa, user.Avatar),
+            };
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+              _configuration["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(15),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        private User Authenticate(UserLogin userLogin)
+        {
+            User user = _userService.LoginUser(userLogin);
+            return user;
+        }
+    }
+}
