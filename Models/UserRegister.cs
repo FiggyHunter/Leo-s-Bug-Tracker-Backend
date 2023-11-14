43c27efb2b7138aedbbeaa54d444130f7fe9cfd2
FiggyHunter <leonardo.roi3@gmail.com>
2023-11-14 21:45:39 +0100

    userRegister model

---
+++ Models/UserRegister.cs
@@ -0,0 +1,20 @@
+﻿namespace BugTrackerAPI.Models
+{
+    public class UserRegister
+    {
+        // Step 1
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Step 2 
+        public string? Role { get; set; }
+        public string? Avatar { get; set; }
+
+        // Step 3
+        public string? Name { get; set; }
+        public override string ToString()
+        {
+            return $"Email: {Email}, Password: {Password}";
+        }
+    }
+}
